06afa2df20edeb5fe10c720d004f0c25b713d5ac
Fihriz Ilham Rabbany <116176265+fihrizilhamr@users.noreply.github.com>
2024-12-18 11:43:07 +0700

    update

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@
 kubectl delete -f mysql/mysql-deployment.yaml \
             -f app/app-deployment.yaml \
             -f phpmyadmin/phpmyadmin-deployment.yaml \
-            -f alpine/alpine-job.yaml \
-            -f ingress.yaml
+            -f alpine/alpine-job.yaml 
+            # -f ingress.yaml
 
 kubectl apply -f mysql/mysql-deployment.yaml \
             -f app/app-deployment.yaml \
             -f phpmyadmin/phpmyadmin-deployment.yaml \
-            -f alpine/alpine-job.yaml \
-            -f ingress.yaml
+            -f alpine/alpine-job.yaml 
+            # -f ingress.yaml
