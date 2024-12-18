5fbdb8ee6dbba3b88cd614591ee1704daec89919
Fihriz Ilham Rabbany <116176265+fihrizilhamr@users.noreply.github.com>
2024-12-18 11:06:22 +0700

    update

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
-bash setup-cluster/kind/1-create-cluster.sh
-bash setup-cluster/kind/2-set-config.sh
-bash setup-cluster/kind/3-install-ingress.sh
-bash setup-cluster/kind/4-cek-ingress.sh
-
+cd setup-cluster/kind/
+bash 1-create-cluster.sh
+bash 2-set-config.sh
+bash 3-install-ingress.sh
+bash 4-cek-ingress.sh
+cd ../..
 
 docker build -t myapp ./app/platform
 kind load docker-image myapp:latest --name mylab99
