b593e52d14698c3341d109b6a53fa016cf268246
Fihriz Ilham Rabbany <116176265+fihrizilhamr@users.noreply.github.com>
2024-12-18 11:59:05 +0700

    update

--- build.sh
+++ build.sh
@@ -14,8 +14,8 @@ docker cp $PWD mylab99-control-plane:/KA-Tugas-5-2024
 docker cp $PWD mylab99-worker:/KA-Tugas-5-2024
 docker cp $PWD mylab99-worker2:/KA-Tugas-5-2024
 
-kubectl delete -f mysql/mysql-deployment.yaml \
-            -f app/app-deployment.yaml \
-            -f phpmyadmin/phpmyadmin-deployment.yaml \
-            -f alpine/alpine-job.yaml 
+# kubectl delete -f mysql/mysql-deployment.yaml \
+#             -f app/app-deployment.yaml \
+#             -f phpmyadmin/phpmyadmin-deployment.yaml \
+#             -f alpine/alpine-job.yaml 
             # -f ingress.yaml
