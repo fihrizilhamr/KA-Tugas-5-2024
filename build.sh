60d64f368fe2a550e9c983495a1d189ae08dca52
Fihriz Ilham Rabbany <116176265+fihrizilhamr@users.noreply.github.com>
2024-12-19 09:55:34 +0700

    update

--- build.sh
+++ build.sh
@@ -9,9 +9,9 @@ docker build -t myapp ./app/platform
 kind load docker-image myapp:latest --name mylab99
 PWD=$(pwd)
 
-docker rm -rf mylab99-control-plane:/KA-Tugas-5-2024
-docker rm -rf mylab99-worker:/KA-Tugas-5-2024
-docker rm -rf mylab99-worker2:/KA-Tugas-5-2024
+docker rm -f mylab99-control-plane:/KA-Tugas-5-2024
+docker rm -f mylab99-worker:/KA-Tugas-5-2024
+docker rm -f mylab99-worker2:/KA-Tugas-5-2024
 
 
 docker cp $PWD mylab99-control-plane:/KA-Tugas-5-2024
