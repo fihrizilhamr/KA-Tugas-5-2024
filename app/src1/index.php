5f88e8c6f216c9b9868afbabf1f3af003a16117a
Fihriz Ilham Rabbany <116176265+fihrizilhamr@users.noreply.github.com>
2024-12-19 08:34:35 +0700

    update

--- app/src1/index.php
+++ app/src1/index.php
@@ -23,6 +23,8 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
 			<td><strong>Age</strong></td>
 			<td><strong>Email</strong></td>
 			<td><strong>Action</strong></td>
+			<td><strong>Username</strong></td>
+			<td><strong>Password</strong></td>
 		</tr>
 		<?php
 		while ($res = mysqli_fetch_assoc($result)) {
@@ -30,6 +32,8 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
 			echo "<td>".$res['name']."</td>";
 			echo "<td>".$res['age']."</td>";
 			echo "<td>".$res['email']."</td>";	
+			echo "<td>".$res['username']."</td>";
+			echo "<td>".$res['password']."</td>";
 			echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | 
 			<a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
 		}
