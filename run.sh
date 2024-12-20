057fd246fa99a2ab839c585f715a901bf4767064
Fihriz Ilham Rabbany <116176265+fihrizilhamr@users.noreply.github.com>
2024-12-20 13:53:30 +0700

    update

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ kubectl apply -f mysql/pvc-deployment.yaml \
             -f phpmyadmin/phpmyadmin-deployment.yaml \
             -f mysql/mysql-deployment.yaml
             # -f ingress.yaml
-
+sleep(30)
 kubectl port-forward pod/$(kubectl get pods -l app=phpmyadmin -o jsonpath='{.items[0].metadata.name}') 55550:80 &
 kubectl port-forward pod/$(kubectl get pods -l app=app -o jsonpath='{.items[0].metadata.name}') 5555:80 &
 kubectl port-forward pod/$(kubectl get pods -l app=app -o jsonpath='{.items[0].metadata.name}') 5:8080
