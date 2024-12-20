8eb5ce9c39aef0823856202fa583f08904ad1287
Fihriz Ilham Rabbany <116176265+fihrizilhamr@users.noreply.github.com>
2024-12-20 08:54:15 +0700

    update

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 # kubectl create configmap mysql-config --from-env-file=.env
 
-kubectl apply -f alpine/alpine-job.yaml \
+kubectl apply -f mysql/pvc-deployment.yaml \
+            -f alpine/alpine-job.yaml \
             -f app/app-deployment.yaml \
             -f phpmyadmin/phpmyadmin-deployment.yaml \
             -f mysql/mysql-deployment.yaml
